d558a98f3ffb84b25c923abfbe4a4c038abeff20
José Miguel Cordero Carvaco <josemcorderoc@gmail.com>
2022-12-25 00:12:14 +0000

    refactor(frontend): add props to Map component and refactor paths

--- frontend/elecciones-chile-visualizer/src/App.svelte
+++ frontend/elecciones-chile-visualizer/src/App.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
-  import ElectoralMap from './lib/ElectoralMap.svelte'
+  import ElectoralMap from './lib/components/ElectoralMap/ElectoralMap.svelte';
+  import data from "./assets/test_votes.json";
+  console.log(data);
+
+  
 </script>
 
 <main>
   <h1>Elecciones Chile Visualizer</h1>
 
-  <ElectoralMap/>
+  <ElectoralMap data={data.data} territorio={"comuna"} entidad={"candidato"}/>
 
 </main>
 
