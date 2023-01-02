5257859f258f2fc40a3c9b991dfdaf3f71bccc26
José Miguel Cordero Carvaco <josemcorderoc@gmail.com>
2023-01-02 18:37:50 +0000

    fix: rename colormap package import

--- frontend/elecciones-chile-visualizer/src/lib/constants.ts
+++ frontend/elecciones-chile-visualizer/src/lib/constants.ts
@@ -2,7 +2,7 @@ import type { FeatureCollection } from "geojson";
 import elecciones_data from "../assets/elecciones.json";
 import comunas_data from "../assets/comunas.json";
 import type { Elections } from "./types";
-import * as colormap from 'colormap';
+import colormap from 'colormap';
 
 export const elections = <Elections>elecciones_data;
 export const comunas = <FeatureCollection>comunas_data;
